effdf29a44fa4a038a4dc47da833526442a84ccd
MJ-DE17 <manasadevi.j@diggibyte.com>
2026-01-18 22:59:04 +0530

    q4 two

--- Question-4/cherry-pick.py
+++ Question-4/cherry-pick.py
@@ -1,1 +1,5 @@
-Using cherrypick in this question 
+print("Using cherrypick in this question ")
+
+print("Using cherrypick in this question ")
+print( "this is 1st commit in this branch")
+print( "this is 2nd commit in this branch")
